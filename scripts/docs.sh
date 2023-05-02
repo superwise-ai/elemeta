157d5acd1aa79b870a1a9bf370d25e15e49399b2
Roei Zavida <roeizavida@users.noreply.github.com>
2023-05-02 23:11:08 +0300

    ci(build): Add build workflow for package and docs

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -ex
 # shellcheck disable=SC2164
 
+# Install docs dependencies
 poetry install --with docs
+
+# Build docs
 poetry run sphinx-apidoc -o ./docs ./elemeta --force
-poetry run make -C docs html
+rm -rf ./docs/_build
+poetry run sphinx-build -M html ./docs ./docs/_build -v --color -w /tmp/sphinx-build.log
+
+# Check for errors in sphinx-build
+SPHINX_ERRORS=$(grep -E '.*: ERROR:.*' /tmp/sphinx-build.log | wc -l | sed -e 's/ //g' || true)
+if [ $SPHINX_ERRORS -gt 0 ]
+then
+  echo "Sphinx build had ${SPHINX_ERRORS} errors. Exiting."
+  exit 1
+fi
