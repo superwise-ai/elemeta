380a1fbf4a18a8c685aea49db3534a09e454b2f7
lior <liorka1313@gmail.com>
2023-10-04 10:19:15 +0300

    refactor(PairMetafeatureExtractorsRunnerResult): will have the name of the extractor near the value

--- elemeta/nlp/runners/pair_metafeature_extractors_runner.py
+++ elemeta/nlp/runners/pair_metafeature_extractors_runner.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, List, Dict
 
 from pydantic import BaseModel
 
@@ -11,9 +11,9 @@ from elemeta.nlp.extractors.low_level.abstract_text_pair_metafeature_extractor i
 
 
 class PairMetafeatureExtractorsRunnerResult(BaseModel):
-    input_1: List[Any]
-    input_2: List[Any]
-    input_1_and_2: List[Any]
+    input_1: Dict[str, Any]
+    input_2: Dict[str, Any]
+    input_1_and_2: Dict[str, Any]
 
 
 class PairMetafeatureExtractorsRunner:
@@ -43,9 +43,16 @@ class PairMetafeatureExtractorsRunner:
             the metafeatures extracted from text
         """
         return PairMetafeatureExtractorsRunnerResult(
-            input_1=[extractor(input_1) for extractor in self.input_1_extractors],
-            input_2=[extractor(input_2) for extractor in self.input_2_extractors],
-            input_1_and_2=[
-                extractor(input_1, input_2) for extractor in self.input_1_and_2_extractors
-            ],
+            input_1={
+                extractor.name: extractor(input_1)
+                for extractor in self.input_1_extractors
+            },
+            input_2={
+                extractor.name: extractor(input_2)
+                for extractor in self.input_2_extractors
+            },
+            input_1_and_2={
+                extractor.name: extractor(input_1, input_2)
+                for extractor in self.input_1_and_2_extractors
+            },
         )
