11d6657a216716a4477a895cdba796bf2c3ce40c
omrihaber <76410724+omrihaber@users.noreply.github.com>
2023-11-21 17:54:43 +0200

    feat(extractor): toxicity handle long text

--- elemeta/nlp/extractors/high_level/toxicity_extractor.py
+++ elemeta/nlp/extractors/high_level/toxicity_extractor.py
@@ -1,5 +1,7 @@
-from typing import Optional
+from functools import partial
+from typing import Callable, List, Optional
 
+from nltk.tokenize import sent_tokenize
 from transformers import (
     AutoModelForSequenceClassification,
     AutoTokenizer,
@@ -10,6 +12,12 @@ from elemeta.nlp.extractors.low_level.abstract_text_metafeature_extractor import
     AbstractTextMetafeatureExtractor,
 )
 
+DEFAULT_TOKENIZER = partial(sent_tokenize, language="english")
+
+
+def _is_toxic(label: str) -> bool:
+    return label == "0-3 stars"
+
 
 class ToxicityExtractor(AbstractTextMetafeatureExtractor):
     """
@@ -19,6 +27,8 @@ class ToxicityExtractor(AbstractTextMetafeatureExtractor):
     def __init__(
         self,
         name: Optional[str] = None,
+        tokenizer: Callable = DEFAULT_TOKENIZER,
+        aggregate: Callable = min,
     ):
         """
         Parameters
@@ -27,10 +37,22 @@ class ToxicityExtractor(AbstractTextMetafeatureExtractor):
             name of the metadata and if not given will extract the name from the class name
         path: Optional[str]
             the path used for the model. If not given, defaults to the hugginface library
+        tokenizer: Optional[callable]
+            the tokenizer to use. If not given, defaults to nltk.sent_tokenize.
+            the model that is used has a restriction of 512 tokens,
+            this method will split the text into chunks
+        aggregate: Optional[callable]
+            the aggregation function to use. If not given, defaults to min.
+            the model that is used has a restriction
+            of 512 tokens, this method will split the text into
+            sentences and run the model on each sentence.
+            the min aggregation is used to get the most toxic sentence.
         """
 
         super().__init__(name)
         self.model_path = "tillschwoerer/roberta-base-finetuned-toxic-comment-detection"
+        self.tokenizer = tokenizer
+        self.aggregate = aggregate
 
     def extract(self, text: str) -> float:
         """
@@ -45,13 +67,16 @@ class ToxicityExtractor(AbstractTextMetafeatureExtractor):
         float
             a float closer to one is more toxic, closer to zero is non toxic.
         """
-        result = 0.0
         toxicity_tokenizer = AutoTokenizer.from_pretrained(self.model_path)
         model = AutoModelForSequenceClassification.from_pretrained(self.model_path)
         pipeline = TextClassificationPipeline(model=model, tokenizer=toxicity_tokenizer)
-        for pair in pipeline(text):
-            if pair["label"] == "0-3 stars":
-                result = pair["score"]
-            else:
-                result = 1 - pair["score"]
-        return result
+        text = self.tokenizer(text)
+        labels_and_scores = pipeline(text)
+
+        results: List[float] = [
+            label_and_score["score"]
+            if _is_toxic(label_and_score["label"])
+            else 1 - label_and_score["score"]
+            for label_and_score in labels_and_scores
+        ]
+        return self.aggregate(results)
